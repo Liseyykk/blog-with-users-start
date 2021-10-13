764164a3e8b0bd9212fd749fb1e1cc86ad9f5af3
Lise YU <little_yk@hotmail.com>
2021-10-13 17:27:41 +1100

    correct environment variable

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ import os
 Base = declarative_base()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'SECRET_KEY'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
